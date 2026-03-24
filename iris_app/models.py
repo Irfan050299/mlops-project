589782c43ce621e49f5fa7ccf2ed4732e4cdecb4
Irfan <iahmedkhan03@gmail.com>
2026-03-24 13:10:26 +0530

    feat: add monitoring and logging

--- iris_app/models.py
+++ iris_app/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
-from django.db import models
-
 class PredictionLog(models.Model):
     features = models.CharField(max_length=200)
     prediction = models.IntegerField()
+    response_time_ms = models.FloatField(default=0)  # ✅ yeh add karo
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.features} -> {self.prediction}"
+
+class ErrorLog(models.Model):  # ✅ yeh add karo
+    error_message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.error_message
--- iris_model/settings.py
+++ iris_model/settings.py
@@ -55,11 +55,20 @@ WSGI_APPLICATION = 'iris_model.wsgi.application'
 
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+# Database
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
